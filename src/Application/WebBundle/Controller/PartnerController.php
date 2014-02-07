191577eabfa4893ee1d4186c254012cd1ea7eef7
Igor Drozd <igdr@ciklum.com>
2014-02-08 00:00:47 +0200

    translate

--- src/Application/WebBundle/Controller/PartnerController.php
+++ src/Application/WebBundle/Controller/PartnerController.php
@@ -18,4 +18,34 @@ class PartnerController extends Controller
 
         return $this->render('ApplicationWebBundle:Partner:index.html.twig');
     }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function ritmoAction()
+    {
+        $this->get("white_october_breadcrumbs")->addItem('page.partner.title', $this->generateUrl('web_partners'))->addItem('page.partner.ritmo.title');
+
+        return $this->render('ApplicationWebBundle:Partner:ritmo.html.twig');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function mempexAction()
+    {
+        $this->get("white_october_breadcrumbs")->addItem('page.partner.title', $this->generateUrl('web_partners'))->addItem('page.partner.mempex.title');
+
+        return $this->render('ApplicationWebBundle:Partner:mempex.html.twig');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function centerplasticAction()
+    {
+        $this->get("white_october_breadcrumbs")->addItem('page.partner.title', $this->generateUrl('web_partners'))->addItem('page.partner.centerplastic.title');
+
+        return $this->render('ApplicationWebBundle:Partner:centerplastic.html.twig');
+    }
 }

1ea8333c36b88b4717ab73c53736be8dd12fd208
Igor Drozd <igdr@ciklum.com>
2014-02-09 23:24:50 +0200

    fix issues with grayscale image

--- src/Application/CoreBundle/Resources/public/js/core.js
+++ src/Application/CoreBundle/Resources/public/js/core.js
@@ -73,29 +73,33 @@ var core = {
 
     // make img grayscale
     grayscaleImage : function (imgObj) {
-        var canvas = document.createElement('canvas');
-        var canvasContext = canvas.getContext('2d');
+        try {
+            var canvas = document.createElement('canvas');
+            var canvasContext = canvas.getContext('2d');
 
-        var imgW = imgObj.width;
-        var imgH = imgObj.height;
-        canvas.width = imgW;
-        canvas.height = imgH;
+            var imgW = imgObj.width;
+            var imgH = imgObj.height;
+            canvas.width = imgW;
+            canvas.height = imgH;
 
-        canvasContext.drawImage(imgObj, 0, 0);
-        var imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
+            canvasContext.drawImage(imgObj, 0, 0);
+            var imgPixels = canvasContext.getImageData(0, 0, imgW, imgH);
 
-        for(var y = 0; y < imgPixels.height; y++){
-            for(var x = 0; x < imgPixels.width; x++){
-                var i = (y * 4) * imgPixels.width + x * 4;
-                var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-                imgPixels.data[i] = avg;
-                imgPixels.data[i + 1] = avg;
-                imgPixels.data[i + 2] = avg;
+            for(var y = 0; y < imgPixels.height; y++){
+                for(var x = 0; x < imgPixels.width; x++){
+                    var i = (y * 4) * imgPixels.width + x * 4;
+                    var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+                    imgPixels.data[i] = avg;
+                    imgPixels.data[i + 1] = avg;
+                    imgPixels.data[i + 2] = avg;
+                }
             }
-        }
 
-        canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-        return canvas.toDataURL();
+            canvasContext.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+            return canvas.toDataURL();
+        } catch (e) {
+            return null;
+        }
     },
 
     /**
